e40ce3a460ceca58e322c652495dd2be6df0c0d5
Thanos Theodoridis <at@atworks.gr>
2012-12-27 17:03:40 +0200

    Finished simple querying via imdbapi

--- imdbq.py
+++ imdbq.py
@@ -1,1 +1,28 @@
-__author__ = 'attheodo'
+#!/usr/bin/python
+import requests
+import argparse
+
+API_ENDPOINT = 'http://imdbapi.org/'
+
+def search_by_title(title):
+    print "\n[-] Querying for \"%s\"\n" % title
+
+    get_params = {'title': title,'type': 'json', 'plot': 'simple', 'limit': 5 ,'yg': 0,'lang': 'en-US', 'mt': 'none', 'release': 'simple'}
+    r = requests.get(API_ENDPOINT,params=get_params)
+    response = r.json()
+
+    for movie in response:
+       print "\t- %s (%s) - %s\n\t\tRating: %s\n\t\t%s\n" % (movie['title'], movie['year'], ', '.join(movie['genres']), movie['rating'],movie.get('plot_simple',None))
+
+def main():
+    parser = argparse.ArgumentParser(description='IMDBq')
+    parser.add_argument('-t','--title',help = 'Exact title of the movie',dest = "title")
+    args = parser.parse_args()
+
+    if args.title:
+        search_by_title(args.title)
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
